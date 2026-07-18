4754ebf4b689dc169241b42aec5703d234202838
toonienguyen <165347760+toonienguyen@users.noreply.github.com>
2026-07-18 16:39:55 +0700

    Update Footer.svelte

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,16 +1,5 @@
 <script lang="ts">
-	import { GITHUB_URL_VERT, DISCORD_URL } from "$lib/util/consts";
-	import { m } from "$lib/paraglide/messages";
-
-	const commitHash =
-		__COMMIT_HASH__ && __COMMIT_HASH__ !== "unknown"
-			? __COMMIT_HASH__
-			: null;
-
 	const year = new Date().getFullYear();
-
-	// we can't use svelte snippets or a derived object to render the footer as it causes a full-page reload
-	// ...for some reason. i have no idea, maybe it's to do with the {#key $locale} in +layout.svelte
 </script>
 
 <footer
@@ -19,40 +8,8 @@
 	<div
 		class="w-full h-full flex items-center justify-center text-muted gap-3 relative"
 	>
-		<p>{m["footer.copyright"]({ year })}</p>
-		<p>•</p>
-		<a
-			class="hover:underline font-normal"
-			href={GITHUB_URL_VERT}
-			target="_blank"
-		>
-			{m["footer.source_code"]()}
-		</a>
-		<p>•</p>
-		<a
-			class="hover:underline font-normal"
-			href={DISCORD_URL}
-			target="_blank"
-		>
-			{m["footer.discord_server"]()}
-		</a>
-		<p>•</p>
-		<a
-			class="hover:underline font-normal"
-			href="/privacy/"
-		>
-			{m["footer.privacy_policy"]()}
-		</a>
-		{#if commitHash}
-			<p>•</p>
-			<a
-				class="hover:underline font-normal"
-				href="{GITHUB_URL_VERT}/commit/{commitHash}"
-				target="_blank"
-			>
-				{commitHash}
-			</a>
-		{/if}
+		<!-- Chỉ giữ lại đúng dòng chữ bản quyền, tự động đổi chữ VERT thành verttools nếu anh thích -->
+		<p>© {year} verttools</p>
 	</div>
 
 	<div
